99a7fc6314bcf33218020822a68ef70ef13e52e7
leonitsch <leonhaussknecht@gmail.com>
2018-03-17 11:32:13 +0100

    adding mult features

--- MatrixTest.java
+++ MatrixTest.java
@@ -1,18 +1,25 @@
 public class MatrixTest{
 
 	public static void main(String[] args){
+		//TODO think of criteria
+		boolean passes = true;
+
+		Matrix add_a = new Matrix(3,3);
+		add_a.randomize();
+		Matrix add_b = new Matrix(3,3);
+		add_b.add(10);
+		add_a.add(b);
+
+
 		Matrix a = new Matrix(3,2);
-		a.print();
-		a.randomize();
-		a.add(10);
+		a.add(4);
 		a.print();
 
-		Matrix b = new Matrix(3,2);
-		b.randomize();
-		b.add(40);
-		b.print();
-		b.add(a);
-		b.print();
+		Matrix b = new Matrix(2,3);
+		b.add(10);
 
+		Matrix c = Matrix.mult(a,b);
+
+		System.out.println(passes);
 	}
 }
